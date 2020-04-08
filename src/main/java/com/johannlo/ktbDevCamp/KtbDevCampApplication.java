35ca2b7d05cc49aed4788bb03cf362ad73ecc70d
johannlo <johannlo15@gmail.com>
2020-04-08 15:34:17 +0700

    Initial Commit

---
+++ src/main/java/com/johannlo/ktbDevCamp/KtbDevCampApplication.java
@@ -0,0 +1,13 @@
+package com.johannlo.ktbDevCamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbDevCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbDevCampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johannlo/ktbDevCamp/KtbDevCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johannlo.ktbDevCamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbDevCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
